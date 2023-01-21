fbfce07a36a620c8e3afd83408abe19e6cc73352
Yazdan Zinati <contact@yazdan.me>
2023-01-21 14:17:57 -0500

    Add website.

---
+++ Website/README.md
@@ -0,0 +1,33 @@
+# yazdanzinati.com
+
+<div align="center">
+
+[![CI](https://github.com/YazdanZ/yazdanzinati.com/actions/workflows/main.yml/badge.svg?branch=master)](https://github.com/YazdanZ/yazdanzinati.com/actions/workflows/main.yml)
+[![Website shields.io](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](https://yazdanzinati.com/)
+
+</div>
+
+My simple and minimal personal website [yazdanzinati.com](https://yazdanzinati.com/). Built mostly with vanilla js, html, and css.
+
+## Running
+
+```
+git clone https://github.com/YazdanZ/yazdanzinati.com.git
+cd yazdanzinati.com
+npm install
+npm run serve
+```
+
+## Building
+
+```
+npm run build
+```
+
+## Deploying to gh-pages
+
+```
+# modify repo url in packages.json and CNAME if using a custom domain
+npm run predeploy
+npm run deploy
+```
---
+++ Website/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "source-map",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/html/index.html",
+      title: "Yazdan Zinati's Homepage",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
