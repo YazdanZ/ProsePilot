409dac7a343bb1a9056a57708f59a77cdba3116c
David Deng <zhiyun.deng@mail.mcgill.ca>
2023-01-21 14:14:42 -0500

    Change to dropdown

--- frontend client/prosepilot.js
+++ frontend client/prosepilot.js
@@ -2,15 +2,19 @@ class MyCustomElement extends HTMLElement {
     constructor() {
       super();
       this.shadow = this.attachShadow({mode: "open"});
-      
     }
     connectedCallback() {
         this.shadow.innerHTML = `
-        <button>
-            <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Google logo">
-            <span>Change Prose</span>
-        </button>
-        <style>button {
+        
+        <select onchange="prose()">
+            <option value="default">Change Prose</option>
+            <option value="reddit">Reddit</option>
+            <option value="academic">Academic</option>
+            <option value="nytimes">New York Times</option>
+        </select>
+
+        <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Google logo">
+        <style>select {
             background-color: #4285F4;
             color: #fff;
             padding: 8px 16px;
@@ -30,7 +34,12 @@ class MyCustomElement extends HTMLElement {
         </style>
     `;
     }
+
+    
   }
   
   customElements.define("my-custom-element", MyCustomElement);
+  function prose() {
+    console.log("Testing dropdown!");
+}
   
